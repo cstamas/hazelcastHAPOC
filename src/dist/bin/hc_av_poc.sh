0eb2d81c76aec68bd3b354d6c62ebaa1be0b9d3d
Sridhar Ramachandran <lambdazen@gmail.com>
2017-02-27 18:18:29 -0500

    Removed param

--- src/dist/bin/hc_av_poc.sh
+++ src/dist/bin/hc_av_poc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath ../conf:../../../target/hazelcastHAPOC-0.1-SNAPSHOT-jar-with-dependencies.jar com.lambdazen.hazelcast.CommandExecutor "$@"
+java -classpath ../conf:../../../target/hazelcastHAPOC-0.1-SNAPSHOT-jar-with-dependencies.jar com.lambdazen.hazelcast.CommandExecutor
